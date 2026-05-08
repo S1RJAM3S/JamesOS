a0f3bf7ceade93a0fda7ab8b097692aeceefa4e4
S1RJAM3S <s1rjam3s2006@gmail.com>
2026-05-09 01:09:55 +0700

    [FEAT] Added setting themes/templates + Moving away from Omarchy's .local/share/ setup (Because I hate decentralised :<)

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -eEo pipefail
 
-export SIRJAMES_PATH="$HOME/.local/share/sirjames"
+export SIRJAMES_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 export SIRJAMES_INSTALL="$SIRJAMES_PATH/install"
 export SIRJAMES_INSTALL_LOG="/var/log/sirjames-install.log"
 export PATH="$SIRJAMES_PATH/bin:$PATH"
