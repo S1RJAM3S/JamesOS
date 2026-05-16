3921d21d74b6233da3ab952302a8961218bf1e86
S1RJAM3S <s1rjam3s2006@gmail.com>
2026-05-16 14:45:07 +0700

    [FEATURE] Symlink script with Stow

--- home/.config/hypr/binds.lua
+++ home/.config/hypr/binds.lua
@@ -14,9 +14,10 @@ for k, d in pairs({ H = "left", J = "down", K = "up", L = "right" }) do
 	hl.bind(mainMod .. " + " .. k, hl.dsp.focus({ direction = d }))
 end
 
-for k = 0, 9 do
-	hl.bind(mainMod .. " + " .. k, hl.dsp.focus({ workspace = k }))
-	hl.bind(mainMod .. " + SHIFT + " .. k, hl.dsp.window.move({ workspace = k }))
+for w = 1, 10 do
+	k = w % 10
+	hl.bind(mainMod .. " + " .. k, hl.dsp.focus({ workspace = w }))
+	hl.bind(mainMod .. " + SHIFT + " .. k, hl.dsp.window.move({ workspace = w }))
 end
 
 hl.bind(mainMod .. " + S", hl.dsp.workspace.toggle_special("magic"))
---
+++ symlink.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[@]}" )" &>/dev/null && pwd )
+STOW_DIR="$SCRIPT_DIR"
+
+HOME_DIRS=(
+    "$HOME/.config",
+    "$HOME/.local/bin"
+)
+
+for dir in "${HOME_DIRS[@]}"; do
+    if [ ! -d "$dir" ]; then
+	echo "[MKDIR] $dir"
+	mkdir -p "$dir"
+    elif [ -L "$dir" ]; then
+	echo "[ERROR] $dir: Symlink!"
+	exit 1
+    fi
+done
+
+stow -v "$@" -d "$STOW_DIR" -t "$HOME" "home"
